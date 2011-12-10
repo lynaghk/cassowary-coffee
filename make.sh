786b32020855dd593a51dca3636f84f36ae28787
Kevin J. Lynagh <kevin@keminglabs.com>
2011-12-10 08:49:16 -0800

    Update build script so CoffeeScript and Closure compiles sources individually.

--- make.sh
+++ make.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 set -e
 
-OUT=out
+COFFEE_IN=src/coffee/
+JS_OUT=out/js/
+COMPILED_FILE=out/complied.js
 #Clean output folder
-rm -rf $OUT
-mkdir $OUT
+rm -rf $COMPILED_FILE $JS_OUT
 
 #Compile CoffeeScript
-find src/coffee -name '*.coffee' | xargs cat > out/out.coffee
 vendor/coffee-script/bin/coffee   \
     --google                      \
     --compile                     \
-    --output $OUT                 \
-    $OUT/out.coffee
+    --output $JS_OUT $COFFEE_IN 
 
 #Run Closure Compiler
 java -jar vendor/closure-compiler.jar                                            \
+    --js_output_file $COMPILED_FILE                                              \
+    --compilation_level ADVANCED_OPTIMIZATIONS                                   \
     --js vendor/base.js                                                          \
     --js vendor/jshashtable.js                                                   \
-    --js $OUT/out.js                                                             \
-    --js_output_file $OUT/compiled.js                                            \
-    --compilation_level ADVANCED_OPTIMIZATIONS                                   \
+    --js $(find $JS_OUT -name '*.js')
+
