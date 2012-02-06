3e077aa2d7f460e5a352e9601d000a0a52357cf8
Kevin J. Lynagh <kevin@keminglabs.com>
2012-02-05 18:27:51 -0800

    WIP: Trying to figure out how to get everything packed into a JAR so that other ClojureScript projects can pull everything in using Lein.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject com.keminglabs/cassowary "0.0.1-SNAPSHOT"
+  :description "Cassowary linear constraint solver with ClojureScript bindings"
+  :source-path "src/cljs"
+  :compile-path "out/js")
