44fa046dd4ed6377a3c7c0ab377866430ff72c30
Kevin J. Lynagh <kevin@keminglabs.com>
2012-02-11 11:33:02 -0800

    Add explicit requires to cljs core.

--- src/cljs/cassowary/core.cljs
+++ src/cljs/cassowary/core.cljs
@@ -1,18 +1,28 @@
 (ns cassowary.core
-  (:refer-clojure :exclude [+ - =]))
+  (:refer-clojure :exclude [+ - =])
+  (:require [Cl :as Cl] ;;Not strictly necessary, but makes me feel better about writing "Cl/" all the time.
+            [Cl.CL :as CL]
+            [Cl.Variable :as Variable]
+            [Cl.Constraint :as Constraint]
+            [Cl.SimplexSolver :as SimplexSolver]
+            [Cl.LinearEquation :as LinearEquation]))
 
 (defn cvar
   ([] (cvar 0))
-  ([val] (js/Cl.Variable. (js/parseFloat val))))
+  ([val] (Cl/Variable. (js/parseFloat val))))
 
-(defn simplex-solver [] (js/Cl.SimplexSolver.))
+(defn simplex-solver [] (Cl/SimplexSolver.))
 
-(defn constrain! [solver constraint] (.addConstraint solver constraint))
+(defn constrain! [solver constraint]
+  (if (instance? Cl/Constraint constraint)
+    (.addConstraint solver constraint)
+    (throw (js/Error. "Called constrain! with something not derived from Cl.Constraint; perhaps you forgot (:refer-clojure :exclude [+ - =]) ?"))))
+
 (defn stay! [solver cvar] (.addStay solver cvar))
 
-(defn contains-cassowary? [& args]
-  (if (some #(or (instance? js/Cl.Variable %)
-                 (instance? js/Cl.LinearExpression %)) args)
+(defn- contains-cassowary? [& args]
+  (if (some #(or (instance? Cl/Variable %)
+                 (instance? Cl/LinearExpression %)) args)
     :cassowary-var
     :number))
 
@@ -24,6 +34,6 @@
 (defmethod = :number [& args] (apply clojure.core/= args))
 (defmethod - :number [& args] (apply clojure.core/- args))
 
-(defmethod + :cassowary-var [& args] (apply js/Cl.CL.Plus args))
-(defmethod = :cassowary-var [a b] (js/Cl.LinearEquation. a b))
-(defmethod - :cassowary-var [& args] (apply js/Cl.CL.Minus args))
+(defmethod + :cassowary-var [& args] (apply CL/Plus args))
+(defmethod = :cassowary-var [a b] (Cl/LinearEquation. a b))
+(defmethod - :cassowary-var [& args] (apply CL/Minus args))
