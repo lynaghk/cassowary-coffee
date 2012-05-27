eabd6018a147e0decc4c6b2773c905f0ce62ce30
Kevin J. Lynagh <kevin@keminglabs.com>
2012-05-26 21:03:37 -0700

    Update make script & JAR to use new lein cljsbuild JavaScript lib conventions.

--- make.sh
+++ make.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 set -e
 
-COFFEE_IN=src/coffee/
-JS_IN=src/js/
-JS_OUT=out/js/
-PKG_OUT=pkg/ #dir that'll be sucked into JAR
-DEBUG_FILE=out/cassowary-debug.js
-CLOSURE_COMPILER=vendor/closure-compiler.jar
-
-#Get Closure compiler, if it doesn't exist.
-if [ ! -f $CLOSURE_COMPILER ]; then
-    echo "Fetching Google Closure compiler..."
-    mkdir -p vendor
-    cd vendor
-    #This -4 forces cURL to use IP4. Without it, cURL gets confused...
-    curl -4 -O http://closure-compiler.googlecode.com/files/compiler-latest.zip
-    unzip -q compiler-latest.zip
-    mv compiler.jar closure-compiler.jar
-    rm -f COPYING README compiler-latest.zip
-    cd ../
-    echo "Closure compiler retrieved successfully."
-fi
+COFFEE_IN=src/coffee
+PKG_OUT=pkg/closure-js/libs/cassowary #dir that'll be sucked into JAR
+JS_OUT=dev_public/js
 
 #Clean output folder
-rm -rf $JS_OUT
+rm -rf $JS_OUT $PKG_OUT
+mkdir -p $JS_OUT
 
 #Compile CoffeeScript
 vendor/coffee-script/bin/coffee   \
@@ -32,24 +16,7 @@ vendor/coffee-script/bin/coffee   \
     --bare                        \
     --output $JS_OUT $COFFEE_IN 
 
-#Copy vendor JS to output path
-mkdir -p $JS_OUT/vendor
-for f in base.js; do
-    cp vendor/$f $JS_OUT/vendor/
-done
-
-#Run Closure Compiler
-#Note that goog base needs to come first since it's needed but not explicitly required by any files
-java -jar vendor/closure-compiler.jar          \
-    --js_output_file $DEBUG_FILE               \
-    --compilation_level WHITESPACE_ONLY        \
-    --formatting PRETTY_PRINT                  \
-    --manage_closure_dependencies true         \
-    --output_manifest manifest.MF              \
-    --js out/js/vendor/base.js                 \
-    --js $(find $JS_OUT -name '*.js' | grep -v base) \
-    --js $JS_IN/requires.js #Requires file makes sure everything is pulled in so we can run tests against the output file.
-
-rm -rf $PKG_OUT
 mkdir -p $PKG_OUT
-cp -r $JS_OUT $PKG_OUT/cassowaryjs
+cp -r $JS_OUT/ $PKG_OUT/
+
+lein jar
--- project.clj
+++ project.clj
@@ -1,4 +1,20 @@
-(defproject com.keminglabs/cassowary "0.1.0"
+(defproject com.keminglabs/cassowary "0.1.1-SNAPSHOT"
   :description "Cassowary linear constraint solver with ClojureScript bindings"
-  :source-path "src/cljs"
-  :resources-path "pkg/")
+  :license {:name "BSD" :url "http://www.opensource.org/licenses/BSD-3-Clause"}
+  
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  
+  :min-lein-version "2.0.0"
+
+  :plugins [[lein-cljsbuild "0.1.10"]]
+
+  :source-paths ["src/clj" "src/cljs"]
+  
+  :resource-paths ["pkg"]
+  
+  :cljsbuild {:builds
+              [{:source-path "src/cljs"
+                :compiler {:output-to "public/cljs_test.js"
+                           :libs ["dev_public/js/Singult.js"]
+                           :optimizations :advanced}
+                :jar false}]})
